b2d5af9f3683856c2c481524d47f7bda31d69678
simpsons01 <ilikekobe01@gmail.com>
2022-11-22 15:40:39 +0800

    login page add forget password modal

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -2,12 +2,21 @@
 import { reactive, ref } from "vue";
 import FormTextInput from "../components/FormTextInput.vue";
 import FormCheckbox from "../components/FormCheckbox.vue";
+import Modal from "../components/Modal.vue";
 
 const form = reactive({
   account: "",
   password: "",
 });
 const isPasswordDisplay = ref(false);
+const forgetPasswordModal = reactive({
+  show: false,
+  form: {
+    account: "",
+  },
+});
+
+const sendPassword = () => {};
 </script>
 
 <template>
@@ -20,7 +29,7 @@ const isPasswordDisplay = ref(false);
         rounded-xl
         p-10
         mx-auto
-        -translate-y-1/4
+        -translate-y-[4rem]
       "
     >
       <div class="flex">
@@ -38,12 +47,18 @@ const isPasswordDisplay = ref(false);
             />
           </div>
 
-          <div class="mt-1 pl-1">
+          <div class="flex justify-between items-center mt-1 pl-1">
             <form-checkbox
               label="顯示密碼"
               :checked="isPasswordDisplay"
               @change="($event) => (isPasswordDisplay = $event.target.checked)"
             />
+            <button
+              @click="forgetPasswordModal.show = true"
+              class="underline underline-offset-1 cursor-pointer"
+            >
+              忘記密碼?
+            </button>
           </div>
           <div class="mt-6">
             <button class="btn w-full text-white bg-black">登入</button>
@@ -51,11 +66,38 @@ const isPasswordDisplay = ref(false);
         </div>
         <div class="divider basis-[1px] bg-gray-300" />
         <div class="flex-auto flex justify-center items-center">
-          <button class="btn bg-black text-white px-16 py-4">加入會員</button>
+          <router-link to="/register" class="btn bg-black text-white px-16 py-4"
+            >加入會員</router-link
+          >
         </div>
       </div>
     </div>
   </div>
+  <modal
+    title="忘記密碼"
+    :show="forgetPasswordModal.show"
+    :on-close-icon-click="() => (forgetPasswordModal.show = false)"
+    :cancel="{
+      text: '取消',
+      handler: () => (forgetPasswordModal.show = false),
+    }"
+    :confirm="{
+      text: '送出',
+      handler: sendPassword,
+    }"
+  >
+    <template v-slot:content>
+      <div class="py-8 px-4">
+        <div>輸入您的帳號</div>
+        <form-text-input
+          class="mt-1"
+          block
+          v-model="forgetPasswordModal.form.account"
+          label="帳號"
+        />
+      </div>
+    </template>
+  </modal>
 </template>
 
 <style scoped></style>
