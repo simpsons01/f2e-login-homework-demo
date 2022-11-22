7c2aee78f6d66c312c993229b92527808b1b6035
simpsons01 <ilikekobe01@gmail.com>
2022-11-22 14:19:49 +0800

    custom tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom1: "rgb(17 17 26 / 10%) 0px 1px 0px, rgb(17 17 26 / 10%) 0px 8px 24px, rgb(17 17 26 / 10%) 0px 16px 48px"
+      }
+    },
   },
   plugins: [],
 }
