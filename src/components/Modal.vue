f8ad9863478d6ef4e787640ce004bb7a5505875a
simpsons01 <ilikekobe01@gmail.com>
2022-11-22 15:40:05 +0800

    update modal transition and control section btn style

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -28,9 +28,9 @@ const props = defineProps({
 </script>
 
 <template>
-  <Transition name="modal-transition">
+  <Transition name="temp">
     <div
-      v-show="show"
+      v-if="show"
       class="
         fixed
         left-0
@@ -53,7 +53,7 @@ const props = defineProps({
           -translate-y-2/4 -translate-x-2/4
           bg-white
           p-4
-          min-w-[400px] min-h-[200px]
+          min-w-[500px] min-h-[200px]
           rounded-lg
           flex flex-col
           content
@@ -77,11 +77,14 @@ const props = defineProps({
           />
         </div>
         <slot name="title" v-bind="{ title }">
-          <div class="text-2xl text-black">
+          <div
+            class="text-2xl text-black"
+            :class="[onCloseIconClick ? 'mt-3' : '']"
+          >
             {{ title }}
           </div>
         </slot>
-        <slot name="content ">
+        <slot name="content">
           <div class="flex-auto p-2" v-bind="{ content }">
             {{ content }}
           </div>
@@ -90,14 +93,14 @@ const props = defineProps({
           <div v-if="cancel || confirm" class="text-center">
             <button
               v-if="cancel"
-              class="btn text-white px-4 py-1 bg-gray-500 mr-2"
+              class="btn text-white px-8 py-2 bg-gray-400 mr-2"
               @click="($event) => cancel.handler($event)"
             >
               {{ cancel.text }}
             </button>
             <button
               v-if="confirm"
-              class="btn text-white px-4 py-1 bg-black"
+              class="btn text-white px-8 py-2 bg-black"
               @click="($event) => confirm.handler($event)"
             >
               {{ confirm.text }}
@@ -109,16 +112,36 @@ const props = defineProps({
   </Transition>
 </template>
 
-<style scoped>
-.modal-transition-active .backdrop,
-.modal-transition-active .backdrop {
+<style>
+.temp-enter-active .backdrop,
+.temp-leave-active .backdrop {
+  transition: 0.3s opacity;
 }
-.modal-transition-active .backdrop,
-.modal-transition-active .backdrop {
+.temp-enter-active .backdrop .content,
+.temp-leave-active .backdrop .content {
+  transition: 0.3s opacity, 0.3s transform;
+  transition-delay: 0.1s;
 }
 
-.modal-transition-from,
-.modal-transition-to {
+.temp-enter-from .backdrop,
+.temp-leave-to .backdrop {
   opacity: 0;
+}
+
+.temp-enter-to .backdrop,
+.temp-leave-from .backdrop {
+  opacity: 1;
+}
+
+.temp-enter-from .backdrop .content,
+.temp-leave-to .backdrop .content {
+  opacity: 0;
+  transform: translateY(-2rem);
+}
+
+.temp-enter-to .backdrop .content,
+.temp-leave-from .backdrop .content {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
